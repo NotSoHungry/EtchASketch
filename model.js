c0a409a73d9c4a7edcd98721773cbe6159b0a6ad
Pawel Bielinski <pawbielinski@gmail.com>
2018-04-12 01:39:53 +0200

    Update model to notify the view when canvas display mode is changed

--- model.js
+++ model.js
@@ -3,9 +3,11 @@ var gameModel = function () {
     var _widthRatio = 48,
         _heightRatio = 48,
         _viewMode = "canvas",
-        _currentBrush = "",
-        _brushMode = "hover",
-        _currentColor = "green",
+        _currentBorderThickness = 1,
+        _currentBorderColor = "black",
+        _currentBrush = "paint",
+        _currentBrushMode = "hover",
+        _currentColor = "black",
         _currentZoom = 1;
 
     return {
@@ -21,8 +23,31 @@ var gameModel = function () {
             return _currentColor;
         },
 
+        get currentBorderThickness() {
+            return _currentBorderThickness
+        },
+
+        set currentBorderThickness(thicknessValue) {
+            _currentBorderThickness = thicknessValue;
+            return view.toogleGridView(this.currentBorderThickness, this.currentBorderColor);
+        },
+
+        get currentBorderColor() {
+            return _currentBorderColor;
+        },
+
+        set currentBorderColor(colorValue) {
+            _currentBorderColor = colorValue;
+            return view.toogleGridView(this.currentBorderThickness, this.currentBorderColor);
+        },
+
         get viewMode() {
             return _viewMode;
+        },
+
+        set viewMode(modeValue) {
+            _viewMode = modeValue;
+            return view.toogleGridView(this.currentBorderThickness, this.currentBorderColor);
         }
     } 
 }
